ea89de9d843d1d625adb575b0bf9089a05c6d0c0
Andrey Krutogolov <andrey.krutogolov@gmail.com>
2015-04-11 23:30:43 +0300

    add files sync only

---
+++ script/sync_severtrans_files_only.cmd
@@ -0,0 +1,29 @@
+echo "set variables"
+set current_dir=%cd%
+echo "current_dir: " %current_dir%
+
+set backup_path=%BACKUP_PATH%
+echo "backup_path: " %backup_path%
+
+set rfile=regiontrans_backup_19h.sql
+set sfile=severtrans_backup_19h.sql
+set rpfile=%current_dir%\regiontrans_backup_19h.sql
+set spfile=%current_dir%\severtrans_backup_19h.sql
+
+set host=%MYSQL_IP_ADDRESS%
+echo "host: " %host%
+set rport=3306
+set sport=3307
+
+cd %current_dir%
+
+echo "copy regiontrans"
+copy /y  %backup_path%%rfile% %rpfile%
+
+echo "copy severtrans"
+copy /y  %backup_path%%sfile% %spfile%
+
+rem mysql --user=dba --password=sql --host=%host% --port=%rport% severtrans < %rpfile%
+rem mysql --user=dba --password=sql --host=%host% --port=%sport% severtrans < %spfile%
+ 
+
